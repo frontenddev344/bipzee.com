c5745015995ba73b4625e96600df43a055153fdf
Dev avology <devavology12@gmail.com>
2024-08-08 11:31:16 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,3 +32,57 @@ $('.subpage-readmore-btn').click(function (e) {
 });
 
 // Read More Functionality JS end
+
+
+
+// //Cookies JS Start
+
+function SetCookie(cookieName, cookieValue, nDays) {
+  "use strict";
+  var today = new Date();
+  var expire = new Date();
+  if (nDays == null || nDays == 0) nDays = 1;
+  expire.setTime(today.getTime() + 3600000 * 24 * nDays);
+  document.cookie =
+    cookieName +
+    "=" +
+    escape(cookieValue) +
+    ";expires=" +
+    expire.toGMTString() +
+    "; path=/";
+}
+function ReadCookie(cookieName) {
+  "use strict";
+  var theCookie = " " + document.cookie;
+  var ind = theCookie.indexOf(" " + cookieName + "=");
+  if (ind == -1) ind = theCookie.indexOf(";" + cookieName + "=");
+  if (ind == -1 || cookieName == "") return "";
+  var ind1 = theCookie.indexOf(";", ind + 1);
+  if (ind1 == -1) ind1 = theCookie.length;
+  return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
+}
+function DeleteCookie(cookieName) {
+  "use strict";
+  var today = new Date();
+  var expire = new Date() - 30;
+  expire.setTime(today.getTime() - 3600000 * 24 * 90);
+  document.cookie =
+    cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
+}
+function AcceptCookies() {
+  SetCookie("sgCookies", true, 30);
+  jQuery("#cookie-bar").hide();
+}
+$(document).ready(function (e) {
+  var toTop = $(
+    "<div id='cookie-bar'><div id='contentcookie'><p>Ez a weboldal cookie-kat használ annak érdekében, hogy a legjobb élményt nyújthassa webhelyünkön. A cookie-k segítenek nekünk közösségi média funkciókat biztosítani,elemezni forgalmunkat és javítani szolgáltatásainkat. Oldalunk további használatával Ön hozzájárul a cookie-k használatához.</p><button id='Cookie' tabindex=1 onclick='AcceptCookies();'>Accept </button><div class='clear'></div></div></div>"
+  ).appendTo("body");
+  if (!ReadCookie("sgCookies")) {
+    //If the cookie has not been set
+    jQuery("#cookie-bar").show();
+  } else {
+    jQuery("#cookie-bar").hide();
+  }
+});
+
+// //Cookies JS Start
